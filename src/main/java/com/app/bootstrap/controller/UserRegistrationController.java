8cd672e5243fc32f07245c5722826d8a841e371a
owner <owner@hp>
2017-07-23 12:47:51 +0900

    ユーザ登録コントローラ修正

--- src/main/java/com/app/bootstrap/controller/UserRegistrationController.java
+++ src/main/java/com/app/bootstrap/controller/UserRegistrationController.java
@@ -1,21 +1,61 @@
 package com.app.bootstrap.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.app.bootstrap.form.UserRegistrationForm;
+import com.app.bootstrap.service.UserRegistrationService;
 
 /**
- * ユーザ登録画面
+ * User Registration.
  */
 @Controller
+@RequestMapping(value = "/user/registration")
 public class UserRegistrationController {
 
-    @RequestMapping(value = "/user/registration", method = RequestMethod.GET)
-    public ModelAndView index() {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject(new Object());
-        modelAndView.setViewName("index");
-        return modelAndView;
+    @Autowired
+    UserRegistrationService userRegistrationService;
+
+    /**
+     * display screen.
+     *
+     * @param model Model
+     * @return view name
+     */
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("form", new UserRegistrationForm());
+        return "index";
+    }
+
+    /**
+     * to registration confirmation.
+     *
+     * @param form UserRegistrationForm
+     * @param result BindingResult
+     * @param redirectAttributes
+     * @param model RedirectAttributes
+     * @return view name
+     */
+    @PostMapping(params = "conf")
+    public String conf(@ModelAttribute("form") @Validated UserRegistrationForm form,
+                    BindingResult result, RedirectAttributes redirectAttributes, Model model) {
+
+        if (result.hasErrors()) {
+            model.addAttribute("form", form);
+            model.addAttribute("message", "入力された値が不正です。");
+            return "index";
+        }
+
+        redirectAttributes.addFlashAttribute("userRegistrationForm", form);
+        return "redirect:/user/registration/conf";
     }
 }
